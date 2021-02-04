dfa39bddb8886bf7be263930d9ae1ad5f20597bf
yurayura420 <tatu19970625@gmail.com>
2021-02-04 17:18:53 +0900

    ストロングパラメーター

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,7 +31,7 @@ class TweetsController < ApplicationController
   end
   private
   def tweet_params
-    params.require(:tweet).permit(:image,:text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :audio, :musician, :music, :text).merge(user_id: current_user.id)
   end  
   def set_tweet
     @tweet = Tweet.find(params[:id])
