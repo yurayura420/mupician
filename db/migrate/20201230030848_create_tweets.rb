5d811d9bba5cc01cd3b0cf94d38eb87386bd5152
yurayura420 <tatu19970625@gmail.com>
2021-02-04 17:10:54 +0900

    マイグレーションファイル

--- db/migrate/20201230030848_create_tweets.rb
+++ db/migrate/20201230030848_create_tweets.rb
@@ -1,9 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :name
-      t.string :text
-      t.text :image
+      t.string :text, null:false
+      t.string :musician, null:false
+      t.string :music, null:false
+      t.text :image, null:false
       t.timestamps
     end
   end
