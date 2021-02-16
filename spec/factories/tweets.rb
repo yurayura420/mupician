c9f00f9e60bf3a206b13fb7210ec645260282af5
yurayura420 <tatu19970625@gmail.com>
2021-02-16 17:00:05 +0900

    tweetの単体モデルテストコード

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :tweet do
+    musician {'a'}
+    music {'a'}
     text {Faker::Lorem.sentence}
     image {Faker::Lorem.sentence}
     association :user 
+    after(:build) do |tweet|
+      tweet.audio.attach(io: File.open('public/audio/test_audio.mp3'), filename: 'test_audio.mp3')
+    end
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -7,11 +7,7 @@ RSpec.describe Tweet, type: :model do
 
   describe 'ツイートの保存' do
     context "正常系" do
-      it "画像とテキストがあれば投稿できる" do
-        expect(@tweet).to be_valid
-      end
-      it "テキストがあれば投稿できる" do
-        @tweet.image = ""
+      it "全てが正しく入力されていれば投稿できる" do
         expect(@tweet).to be_valid
       end
     end
@@ -20,7 +16,27 @@ RSpec.describe Tweet, type: :model do
         @tweet.text = ""
         @tweet.valid?
         expect(@tweet.errors.full_messages).to include "Text can't be blank"
+      end
+      it "musicianが空では投稿できない" do
+        @tweet.musician = ""
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include "Musician can't be blank"
       end     
+      it "musicが空では投稿できない" do
+        @tweet.music = ""
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include "Music can't be blank"
+      end     
+      it "imageが空では投稿できない" do
+        @tweet.image = ""
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include "Image can't be blank"
+      end  
+      it "audioが空では投稿できない" do
+        @tweet.audio = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include "Audio can't be blank"
+      end    
       it "ユーザーが紐付いていなければ投稿できない" do
         @tweet.user = nil
         @tweet.valid?
